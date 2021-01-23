2a59f283952294b771d77535c8bb0f607e537f0b
RobinDev <contact@piedweb.com>
2021-01-23 16:41:25 +0000

    rewrite installer

--- src/PostInstall.php
+++ src/PostInstall.php
@@ -2,64 +2,45 @@
 
 namespace Pushword\Installer;
 
-use Composer\DependencyResolver\Operation\InstallOperation;
-use Composer\Installer\PackageEvent;
+use Composer\Script\Event;
 use Exception;
 use Symfony\Component\Filesystem\Filesystem;
 
+if (! class_exists(Filesystem::class)) {
+    require_once 'vendor/symfony/filesystem/Filesystem.php';
+}
+
 class PostInstall
 {
-    public static function postPackageInstall(PackageEvent $event): void
+    public static function postUpdateCommand(): void //Event $event
     {
-        /** @var InstallOperation $operation */
-        $operation = $event->getOperation();
+        $packages = array_filter(scandir('vendor/pushword'), function ($package) { return ! \in_array($package, ['.', '..']); });
 
-        $installer = 'vendor/'.$operation->getPackage()->getName().'/src/post-package-install.php';
-        if (file_exists($installer)) {
-            echo '~ Executing '.$operation->getPackage()->getName().' installer.'.\chr(10);
-            include $installer;
-        }
-    }
+        foreach ($packages as $package) {
+            if (file_exists('vendor/pushword/'.$package) && ! file_exists('var/installer/'.md5($package))) {
+                $installer = 'vendor/pushword/'.$package.'/install.php';
+                if (file_exists($installer)) {
+                    echo '~ Executing '.$package.' post update command install action.'.\chr(10);
+                    include $installer;
+                }
 
-    public static function postUpdateCommand(): void
-    {
-        $files = [
-            'templates/base.html.twig',
-            'config/packages/security.yaml',
-            'config/packages/vich_uploader.yaml',
-            'config/packages/sonata_admin.yaml',
-            'config/packages/translation.yaml',
-        ];
-
-        foreach ($files as $file) {
-            if (file_exists($file)) {
-                self::remove($file);
+                self::dumpFile('var/installer/'.md5($package), 'done');
             }
-        }
-
-        if (file_exists('vendor/pushword/core') && ! file_exists('var/installer/pushword-core')) {
-            self::remove('packages.json');
-            self::remove('assets');
-            self::mirror('vendor/pushword/skeleton/assets', 'assets');
-            self::dumpFile('var/installer/pushword-core', 'done');
         }
     }
 
     public static function mirror(string $source, string $dest)
     {
-        require_once 'vendor/symfony/filesystem/Filesystem.php';
         (new Filesystem())->mirror($source, $dest);
     }
 
     public static function remove($path)
     {
-        require_once 'vendor/symfony/filesystem/Filesystem.php';
         (new Filesystem())->remove($path);
     }
 
     public static function dumpFile(string $path, string $content)
     {
-        require_once 'vendor/symfony/filesystem/Filesystem.php';
         (new Filesystem())->dumpFile($path, $content);
     }
 
@@ -69,15 +50,26 @@ class PostInstall
         if (false === $content) {
             throw new Exception('`'.$file.'` not found');
         }
-
-        $content = str_replace($search, $replace, $content);
+        $count = 0;
+        $content = str_replace($search, $replace, $content, $count);
+        if (1 !== $count) {
+            throw new Exception('Error on replacing `'.$search.'` by `'.$replace.'`');
+        }
         file_put_contents($file, $content);
     }
 
     public static function addOnTop(string $file, string $toAdd): void
     {
         $content = (string) @file_get_contents($file);
+        if (false !== strpos($content, $toAdd)) {
+            return;
+        }
         $content = $toAdd.$content;
         self::dumpFile($file, $content);
+    }
+
+    public static function isRoot(): bool
+    {
+        return file_exists('vendor');
     }
 }
